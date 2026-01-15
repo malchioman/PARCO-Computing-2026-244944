0c0457b51fabaa2d308a86f5cc9ef336648e2c5b
malchioman <massimo.malchiodi@studenti.unitn.it>
2026-01-15 15:50:12 +0100

    basis for second deliverable

--- mmio/mmio.c
+++ mmio/mmio.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#include "../code/mmio.h"
+#include "mmio.h"
 
 int mm_read_unsymmetric_sparse(const char *fname, int *M_, int *N_, int *nz_,
                                double **val_, int **I_, int **J_)
